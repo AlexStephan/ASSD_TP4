9eebdad82e9bf8436d5a7a424c8a2039fdd00712
alero <amoldovan@itba.edu.ar>
2021-06-17 18:24:47 -0300

    adding things

--- src/backend/arma_point.py
+++ src/backend/arma_point.py
@@ -1,10 +1,12 @@
 from numpy import ndarray
 
+from src.backend.track_manager import fragment,fragment_list,track
+
 class ARMAPoint(object):
     def __init__(self):
         print("ARMAPoint created")
 
-    def set_fragment_and_calculate_arma(self, fragment):
+    def set_fragment_and_calculate_arma(self, fragm : fragment):
         print("ARMAPoint set")
 
     def get_fundamental_frequency(self) -> float:
@@ -14,6 +16,6 @@ class ARMAPoint(object):
     def synthesize_from_fundamental_frequency(self, fundamental: float):
         print("ARMAPoint synthesize")
 
-    def get_synthesized_fragment(self) -> ndarray:
+    def get_synthesized_fragment(self) -> fragment:
         print("ARMAPoint get synthesize fragment")
-        return ndarray([0])
+        return fragment([0])
--- src/backend/track_manager.py
+++ src/backend/track_manager.py
@@ -1,22 +1,32 @@
 from numpy import ndarray
+from typing import List
+
+
+fragment = ndarray
+fragment_list = List[fragment]
+track = ndarray
 
 
 class TrackManager(object):
     def __init__(self):
         print("TrackManager created")
-        self.track = ndarray([0])
+        self.trck = track([0])
 
-    def set_track(self, track: ndarray):
+    def set_track(self, trck: track):
         print("TrackManager: set_track")
-        self.track = track
+        self.trck = trck
 
-    def get_track(self) -> ndarray:
+    def get_track(self) -> track:
         print("TrackManager: get_track")
-        return ndarray([0])
+        return track([0])
 
     def open_sound_file(self, path: str):
         print("TrackManager: open_sound_file")
-        self.set_track(ndarray([0]))
+        self.set_track(track([0]))
 
     def save_sound_file(self, path: str):
         print("TrackManager: save_sound_file")
+
+    def get_fragment_list(self) -> fragment_list:
+        print("TrackManager: get_fragment_list")
+        return []
