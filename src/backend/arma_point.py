c84fc1c38805f28f2acf25d4425ad3cbd769eac9
alero <amoldovan@itba.edu.ar>
2021-06-17 17:50:06 -0300

    adding things

--- src/backend/arma_point.py
+++ src/backend/arma_point.py
@@ -2,3 +2,10 @@
 class ARMAPoint(object):
     def __init__(self):
         print("ARMAPoint created")
+
+    def set_fragment_and_calculate_arma(self, fragment):
+        print("ARMAPoint set")
+
+    def get_fundamental_frequency(self) -> float:
+        print("get_fundamental_frequency")
+        return 0
--- src/backend/fragment_functions.py
+++
@@ -1,4 +0,0 @@
-
-def get_fundamental_from_fragment(self, fragment) -> float:
-    print("get_fundamental_from_fragment")
-    return 0
