7377d7b31cc99d08ea571ab97df8632ba76211d2
alero <amoldovan@itba.edu.ar>
2021-06-18 01:50:36 -0300

    o

---
+++ src/backend2/my_filter.py
@@ -0,0 +1,38 @@
+from typing import List
+from numpy import ndarray
+import numpy as np
+import scipy.signal as ss
+
+coeff = List
+
+
+class MyFilter(object):
+    def __init__(self, coefficients: coeff = None):
+        print("MyFilter created")
+        self.coefficients = None
+        self.zeros = None
+        self.poles = None  # La estrella del show
+        self.gain = None
+        self.degree = None
+        self.set_coefficients(coefficients)
+
+        self.angles = None
+
+    def set_coefficients(self, coefficients: coeff):
+        self.coefficients = coefficients
+        if self.coefficients is not None:
+            self.degree = len(self.coefficients)
+            denominator = [-1]
+            denominator.extend(self.coefficients)
+            self.zeros,self.poles,self.gain = ss.tf2zpk([1],denominator)
+
+            self.angles = []
+            for p in self.poles:
+                self.angles.append(np.angle(p))
+            self.angles.sort()
+
+    def get_coefficients(self) -> coeff:
+        return self.coefficients
+
+    def get_angles(self) -> List:
+        return self.angles
