6823c831f7985688e7990e060597c37735aa08b9
Gian-ITBA <gmuscariello@itba.edu.ar>
2021-06-18 00:50:30 -0300

    Assemble First Try

--- src/backend/track_manager.py
+++ src/backend/track_manager.py
@@ -6,14 +6,17 @@ import scipy.signal as ss
 import numpy as np
 
 fragment = ndarray
-fragment_list = list[fragment]()
+fragmented_list = list[fragment]()
 track = ndarray
 
 ##############
 # Constantes #
 ##############
-ovrlp = 1
-N = 3
+path = "..\\..\\resources\\wav_files\\Arctic Monkeys - Do I Wanna Know.wav"
+filename = "Test.wav"
+ovrlp = 0
+N = 8192
+fs = 44000
 
 
 
@@ -21,7 +24,8 @@ class TrackManager(object):
     def __init__(self):
         print("TrackManager created")
         self.trck = track([0])
-        self.fragment_list = fragment_list
+        self.fragmented_list = fragmented_list
+        self.new_track = [[0,0]]
 
     def set_track(self, trck: track):
         print("TrackManager: set_track")
@@ -33,28 +37,37 @@ class TrackManager(object):
 
     def open_sound_file(self, path: str):
         print("TrackManager: open_sound_file")
-        samplerate, data = wavfile.read(path)
+        samplerate, data = wavfile.read(path,mmap=False)
         self.set_track(data)
 
-    def save_sound_file(self, path: str):
+    def save_sound_file(self, filename: str):
         print("TrackManager: save_sound_file")
+        wavfile.write(filename, fs, self.new_track)
 
     def chunks(self, N, ovrlp):
         for i in range(0, len(self.trck), N-ovrlp):
-            self.fragment_list.append(self.trck[i:i+N].copy())
-        for i in range(0, len(self.fragment_list)):
-            self.fragment_list[i].resize(N)
+            self.fragmented_list.append(self.trck[i:i+N])
+        for i in range(0, len(self.fragmented_list)):
+            self.fragmented_list[i] = np.pad(self.fragmented_list[i],(0,N-len(self.fragmented_list[i])))
 
-    def get_fragment_list(self) -> fragment_list:
-        print("TrackManager: get_fragment_list")
+    def fragment_list(self):
         self.chunks(N,ovrlp)
-        print(self.fragment_list)
-        for i in range(0, len(self.fragment_list)):
-            self.fragment_list[i] = Window.blackman(self.fragment_list[i])
-        return self.fragment_list
+        # for i in range(0, len(self.fragmented_list)):
+        #     self.fragmented_list[i] = Window.blackman(self.fragmented_list[i])
 
-    def set_fragent_list_and_assemble(self, fragment_list):
-        print("TrackManager: set_fragent_list_and_assemble")
+    def get_fragmented_list(self) -> fragment_list:
+        print("TrackManager: get_fragmented_list")
+        return self.fragmented_list
+
+    def set_fragment_list_and_assemble(self, fragmented_list):
+        print("TrackManager: set_fragment_list_and_assemble")
+        print(len(self.fragmented_list))
+        for i in range(0, len(self.fragmented_list)-1):
+            print(self.new_track)
+            print(i)
+            print(self.fragmented_list[i])
+            self.new_track = np.concatenate((self.new_track, self.fragmented_list[i]))
+
 
 
 class Window(object):
@@ -77,5 +90,9 @@ class Window(object):
 if __name__ == '__main__':
     A = TrackManager()
     ar = np.array([1,2,3,4,5,6,7,8,9,10])
-    A.set_track(ar)
-    A.get_fragment_list()
+    # A.set_track(ar)
+    A.open_sound_file(path)
+    A.fragment_list()
+    B = A.get_fragmented_list()
+    A.set_fragment_list_and_assemble(B)
+    A.save_sound_file(filename)
